b02e64711313100a6919ed08f8c93c30988212ff
cc <ycc843092012@gmail.com>
2025-08-07 10:31:29 +0800

    增加ecoprop REA服务器测试环境

--- config.py
+++ config.py
@@ -2,24 +2,24 @@
 import os
 
 class Config:
-    # SECRET_KEY = os.urandom (24)
-    # MYSQL_USER = 'root'
-    # MYSQL_PASS = 'ycc962464'
-    # MYSQL_HOST = '127.0.0.1'
-    # MYSQL_PORT = '3306'
-    # MYSQL_DB = 'fastapi_models'
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ycc962464@127.0.0.1:3306/model_file'
-    # SQLALCHEMY_COMMIT_ON_TEARDOWN =True
-    # SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # email_host = 'smtp.qq.com'  # 设置服务器
-    # mail_pwd = "zlamhlqmvtbybcgb"  # 口令
-    # email_receivers = '843092012@qq.com'
-    # SECRET_KEY = 'abcdefg'
-    # # UPLOAD_FOLDER = r'\my_pro\python\mixgo_model_file\file'
-    # UPLOAD_FOLDER = R'\home\upload'
-    # # JSONIFY_MIMETYPE = "application/json"
-    # JSON_AS_ASCII = False  # support Chinese
-    # JSON_SORT_KEYS = False  # don't sort when jsonify （ref）
+
+    # API path ===============================================================================
+    # PND API 路径
+    PND_PROJECT_INFO = "/pnd-api/project/queryProjectInfoById"
+    PND_PDF_PROJECT_LIST = "/pnd-api/pdf/queryPdfProjectList"
+    PND_PDF_DISTRICT_LIST = "/pnd-api/pdf/queryPdfDistrictList"
+    PND_PROJECT_RETAIL_COUNT = "/pnd-api/project/queryRetailCount"
+    PND_PROJECT_INFO_BIG = "/pnd-api/project/queryProjectInfoById_big"
+
+    # APP Service API 路径
+    APP_PROJECT_BEDROOM_COUNT = "/app-service/project/queryProjectCountByBedroom"
+    APP_AGENT_SHARE_INFO = "/app-service/agent/queryShareAgentInfo"
+    APP_PROJECT_INFO = "/app-service/project/queryProjectInfo"
+    APP_UNIT_TYPE_REPORT = "/app-service/unit/unitTypeReport"
+    APP_SITEPLAN_IMG = "/app-service/siteplan/querySitePlanImg"
+    APP_MEDIA_SHARE = "/app-service/media/queryProjectShareMedia"
+    APP_FLOOR_PLANS = "/app-service/floor/queryFloorPlansByType"
+    APP_UNIT_INFO = "/app-service/unit/getUnitInfo"
 
 
 
@@ -60,50 +60,58 @@ class Config:
     # ecoprop_return_path = "/home/upload/broke/ecoprop/pdf"
 
 
-    # 香港独立部署程序 ===============================================================================
+    # # 香港独立部署测试 ===============================================================================
+    # envs = "release" # 发布
+    # imgpath = 'https://ecoprop.s3.ap-southeast-1.amazonaws.com' # oss 图片
+    # host_imgpath = 'http://imagesingmap.mypropdb.com' # 服务所在的本地图片 /upload 地址 
+
+    # urlpath = 'http://127.0.0.1:9998' #API
+    # now_host = "http://127.0.0.1:9998"
+
+    # # ReLoSG 项目API地址
+    # ReLoSG_HOST = "http://192.168.0.145:4501" 
+
+    # # wkhtmltopdf 的路径
+    # wkhtml_path = R"/usr/bin/wkhtmltopdf"
+
+    # filepath = os.getcwd()  # 项目所在路径 
+    # returnpaths = "/home/upload/savills/pnd/file/report" # Pnd 报告保存路径
+    # ecoprop_temp_path = os.path.join(os.getcwd(),'temp') # 项目模板所在路径 
+    # ecoprop_return_path = "/home/upload/savills/ecoprop/pdf" # Ecoprop 报告保存路径
+
+
+    # # Link 地址
+    # ecoprop_share_path = "http://share.ecoprop.com/" # # Ecoprop 分享路径
+    # ecoprop_share_paths = "https://share.ecoprop.com/" # # https 
+    # ecoprop_pro_vs_path = "https://app.singmap.com/share/index.html#/vsProject" # Ecoprop Pro 路径
+
+    # 服务迁移到新服务器测试 ===============================================================================
     envs = "release" # 发布
     imgpath = 'https://ecoprop.s3.ap-southeast-1.amazonaws.com' # oss 图片
-    host_imgpath = 'http://imagesingmap.mypropdb.com' # 服务所在的本地图片 /upload 地址 
+    host_imgpath = 'http://imagedev.singmap.com' # 服务所在的本地图片 /upload 地址 
 
     urlpath = 'http://127.0.0.1:9998' #API
     now_host = "http://127.0.0.1:9998"
 
-    # ReLoSG 项目API地址
-    ReLoSG_HOST = "http://192.168.0.145:4501" 
+    # # ReLoSG 项目API地址
+    # ReLoSG_HOST = "http://192.168.0.145:4501" 
 
     # wkhtmltopdf 的路径
     wkhtml_path = R"/usr/bin/wkhtmltopdf"
 
     filepath = os.getcwd()  # 项目所在路径 
-    returnpaths = "/home/upload/savills/pnd/file/report" # Pnd 报告保存路径
     ecoprop_temp_path = os.path.join(os.getcwd(),'temp') # 项目模板所在路径 
+    # 文件导出路径
+    returnpaths = "/home/upload/savills/pnd/file/report" # Pnd 报告保存路径
     ecoprop_return_path = "/home/upload/savills/ecoprop/pdf" # Ecoprop 报告保存路径
 
-
     # Link 地址
-    ecoprop_share_path = "http://share.ecoprop.com/" # # Ecoprop 分享路径
-    ecoprop_share_paths = "https://share.ecoprop.com/" # # https 
-    ecoprop_pro_vs_path = "https://app.singmap.com/share/index.html#/vsProject" # Ecoprop Pro 路径
+    ecoprop_share_path = "http://sharedev.ecoprop.com/" # Ecoprop 分享路径
+    ecoprop_share_paths = "https://sharedev.ecoprop.com/" # # https 
+    ecoprop_pro_vs_path = "https://appdev.singmap.com/share/index.html#/vsProject" # Ecoprop Pro 路径
 
 
 
-    # API path ===============================================================================
-    # PND API 路径
-    PND_PROJECT_INFO = "/pnd-api/project/queryProjectInfoById"
-    PND_PDF_PROJECT_LIST = "/pnd-api/pdf/queryPdfProjectList"
-    PND_PDF_DISTRICT_LIST = "/pnd-api/pdf/queryPdfDistrictList"
-    PND_PROJECT_RETAIL_COUNT = "/pnd-api/project/queryRetailCount"
-    PND_PROJECT_INFO_BIG = "/pnd-api/project/queryProjectInfoById_big"
-
-    # APP Service API 路径
-    APP_PROJECT_BEDROOM_COUNT = "/app-service/project/queryProjectCountByBedroom"
-    APP_AGENT_SHARE_INFO = "/app-service/agent/queryShareAgentInfo"
-    APP_PROJECT_INFO = "/app-service/project/queryProjectInfo"
-    APP_UNIT_TYPE_REPORT = "/app-service/unit/unitTypeReport"
-    APP_SITEPLAN_IMG = "/app-service/siteplan/querySitePlanImg"
-    APP_MEDIA_SHARE = "/app-service/media/queryProjectShareMedia"
-    APP_FLOOR_PLANS = "/app-service/floor/queryFloorPlansByType"
-    APP_UNIT_INFO = "/app-service/unit/getUnitInfo"
 
     @staticmethod
     def init_app(app):
--- modules/Transaction/__init__.py
+++ modules/Transaction/__init__.py
@@ -61,25 +61,155 @@ async def SetMapping(data: get_siteplan):
 
 
 
+class Contract(BaseModel):
+    # 买家信息
+    BuyerName1: str = '' # 买家姓名
+    BuyerName2: str = ''
+    BuyerName3: str = ''
+    BuyerName4: str = ''
+    BuyerName5: str = ''
+    BuyerId1: str = '' # 买家身份证号
+    BuyerId2: str = ''
+    BuyerId3: str = ''
+    BuyerId4: str = ''
+    BuyerId5: str = ''
+    BuyerNationality1: str = '' # 买家国籍
+    BuyerNationality2: str = ''
+    BuyerNationality3: str = ''
+    BuyerNationality4: str = ''
+    BuyerNationality5: str = ''
+    BuyerAddress1: str = '' # 买家地址
+    BuyerAddress2: str = ''
+    BuyerAddress3: str = ''
+    BuyerAddress4: str = ''
+    BuyerAddress5: str = ''
+    BuyerBusinessNumber1: str = '' # 买家商业登记号码
+    BuyerBusinessNumber2: str = ''
+    BuyerBusinessNumber3: str = ''
+    BuyerBusinessNumber4: str = ''
+    BuyerBusinessNumber5: str = ''
+    BuyerOccupation1: str = '' # 买家职业
+    BuyerOccupation2: str = ''
+    BuyerOccupation3: str = ''
+    BuyerOccupation4: str = ''
+    BuyerOccupation5: str = ''
+    # 合同信息
+    signData: str = '' # 签署日期
+    signPlace: str = '' # 签署地点
+    contractNumber: str = '' # 合同编号
+    contractName: str = '' # 合同名称
+    contractType: str = '' # 合同类型
+    # 律师信息
+    lawyerName: str = '' # 律师姓名
+    lawyerId: str = '' # 律师身份证号
+    lawyerNationality: str = '' # 律师国籍
+    lawyerAddress: str = '' # 律师地址
+    lawyerBusinessNumber: str = '' # 律师商业登记号码
+    LawyerEmail: str = '' # 律师邮箱
+    # 房产信息
+    projectName: str = '' # 房产项目名称
+    projectAddress: str = '' # 房产项目地址
+    buildingName: str = '' # 房产楼栋名称
+    buildingNumber: str = '' # 房产楼栋编号
+    unitName: str = '' # 房产单元名称
+    unitType: str = '' # 房产户型
+    unitArea: str = '' # 房产面积
+    unitPrice: str = '' # 房产单价
+    totalPrice: str = '' # 房产总价
+    brokerage: str = '' # 中介费
+    brokerageRate: str = '' # 中介费率
+    paymentMethod: str = '' # 付款方式
+    paymentSchedule: str = '' # 付款时间表
+    paymentAmount: str = '' # 付款金额
+    paymentDate: str = '' # 付款日期
+    
+class Document(BaseModel):
+    # 合同内容填充参数
+    userId: str = ''
+    token: str = ''
+    brokeId: str
+    documentId: str
+    TmpPath: str
+    docPath: str = ''  # 临时文件路径
+    Content: Contract
 
 
 
 
+# 合同内容动态填充
+@router.get('/contract')
+def Contract_Content(data: Document):
+    """
+    合同内容动态填充
+    """
+    try:
+        # print(data)
+        # print(data.model_dump())
+        content = pic.Contract_Content(data.model_dump())
+        content = re.sub("'", '"', '%s' % content)  # 将单引号换成双引号
+        content = re.sub("\n", '', '%s' % content)  # 去除换行符
+        return content
+    except BaseException as e:
+        logger.error('合同内容填充失败=====>{0}'.format(e))
+        raise HTTPException(status_code=404, detail="Contract Content Error")
 
 
 
 
+# #  多字段替换工具
+# from typing import Dict, List, Optional,Callable
+# class ReMultiReplacer:
+#     """基于re模块的多字段替换工具，支持占位符格式和字段排除"""
+    
+#     def __init__(self, placeholder_pattern: str = r'{{\s*(\w+)\s*}}'):
+#         """
+#         初始化替换器
+#         :param placeholder_pattern: 占位符正则模式，默认为{{field}}格式
+#         """
+#         self.pattern = re.compile(placeholder_pattern)
+    
+#     def replace(self, 
+#                 text: str, 
+#                 data: Dict[str, str], 
+#                 exclude_fields: List[str] = None,
+#                 missing_handler: Callable  = None ) -> str:
+#         """
+#         替换文本中的多个字段
+#         :param text: 原始文本
+#         :param data: 替换数据字典 {字段名: 替换值}
+#         :param exclude_fields: 需要排除的字段列表
+#         :param missing_handler: 处理缺失字段的函数，默认保留原始占位符
+#         :return: 替换后的文本
+#         """
+#         exclude_fields = exclude_fields or []
+        
+#         def _replace_match(match):
+#             # 获取匹配到的字段名（如从{{company}}中提取company）
+#             field = match.group(1)
+            
+#             # 处理排除字段
+#             if field in exclude_fields:
+#                 return match.group(0)  # 返回原始占位符
+            
+#             # 处理存在的字段
+#             if field in data:
+#                 return str(data[field])
+            
+#             # 处理缺失的字段
+#             if missing_handler:
+#                 return missing_handler(field, match.group(0))
+#             return match.group(0)  # 默认保留原始占位符
+        
+#         return self.pattern.sub(_replace_match, text)
 
+# # text = "客户：{{customer_name}}（等级：{{customer_level}}），缺失：{{customer_level1}}"
+# # replacer = ReMultiReplacer()
+# # data = {"customer_level": "A","customer_name":"444"} 
+# # exclude_fields = ["customer_level"]
+# # def missing_handler(field, placeholder):
+# #     return f"{placeholder}（未定义）"
 
-
-
-
-
-
-
-
-
-
+# # result2 = replacer.replace(text, data, exclude_fields,missing_handler)
 
 
 
